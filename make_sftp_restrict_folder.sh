ede4099aae6db389949f5dd82df2554337769cfa
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2022-12-11 21:48:55 +0700

    Add files via upload

---
+++ make_sftp_restrict_folder.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+shopt -s dotglob
+shopt -s nullglob
+
+addgroup restriction
+
+#useradd -m store
+
+echo -e "td123456789\ntd123456789" | passwd store
+
+usermod -a -G  restriction store
+
+mkdir /var/res
+
+chgrp -R restriction /var/res
+
+mkdir /var/res/share
+
+chgrp -R restriction /var/res/share
+
+chown store /var/res/share
+
+mkdir /var/res/backup
+
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+
+echo '#Subsystem sftp internal-sftp' >> /etc/ssh/sshd_config
+echo 'Match Group restriction' >> /etc/ssh/sshd_config
+echo '  ChrootDirectory /var/res' >> /etc/ssh/sshd_config
+echo '  ForceCommand internal-sftp' >> /etc/ssh/sshd_config
+echo '  AllowTcpForwarding no' >> /etc/ssh/sshd_config
+echo '  X11Forwarding no' >> /etc/ssh/sshd_config
+echo '  PermitTunnel no' >> /etc/ssh/sshd_config
+
+reboot
+
+
+

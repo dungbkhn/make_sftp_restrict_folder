2793c54fa68eaa524252be695c2b0d5bce46d716
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2022-12-12 08:02:03 +0700

    Add files via upload

---
+++ install_com.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+shopt -s dotglob
+shopt -s nullglob
+
+curdir=$(pwd)
+
+echo $curdir
+
+cd ~
+
+rs=$(ip -6 address | grep 'scope global')
+
+if [ "$rs" == "" ] ; then 
+	echo "No Internet Access, Abort!!!!"
+	exit 1
+fi
+
+git clone https://github.com/dungbkhn/ChangeRemoteInfo.git
+
+cd ./ChangeRemoteInfo
+
+apt install nodejs npm
+
+npm install multer express find
+
+chmod 755 ./changepw.sh
+
+cd ~
+
+git clone https://github.com/dungbkhn/CreateRemoteLink.git
+
+cd ./CreateRemoteLink
+
+npm install multer express find
+
+chmod 755 ./getipv6addr.sh
+
+cd ~
+
+git clone https://github.com/dungbkhn/autotox.git
+
+cd ./autotox
+
+apt install libtoxcore-dev
+
+make clean
+
+make
+
+echo "-----------------------------"
+echo "OK, all coms are installed"
+echo "-----------------------------"
+
+cd $curdir
+
+exit 0
+

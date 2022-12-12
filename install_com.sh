448b5e27607bac3d827ee98a79cb87d13577ed56
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2022-12-12 14:58:25 +0700

    Update install_com.sh

--- install_com.sh
+++ install_com.sh
@@ -122,6 +122,8 @@ text=$an
 
 sed -i.backup '/127.0.0.1/ s/127.0.0.1/'"$text"'/' /root/.config/syncthing/config.xml
 
+rm -r /home/dungnt/Sync
+
 echo "-----------------------------"
 echo "OK, all coms are installed"
 echo "-----------------------------"
